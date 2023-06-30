c5af9a76dea99ae177e1d6db05e583e87328227c
horodeski <geovana.horodeski06@gmail.com>
2023-06-30 19:15:33 -0300

    :sparkles: feat: criação da model autor

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Categoria
+from .models import Categoria, Autor
 
 admin.site.register(Categoria)
+admin.site.register(Autor)
--- livraria/models.py
+++ livraria/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
+
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
-    
+
     def __str__(self):
         return self.descricao
+
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = "Autor"
+        verbose_name_plural = "Autores"
