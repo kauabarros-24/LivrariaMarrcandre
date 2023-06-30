47376d636d3bf73aaf4388f442d5e9d09b8c53f6
horodeski <geovana.horodeski06@gmail.com>
2023-06-30 19:30:13 -0300

    :sparkles: feat: criação da model livro

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from livraria.models import Categoria, Editora, Autor
+from livraria.models import Autor, Categoria, Editora, Livro
 
 admin.site.register(Autor)
 admin.site.register(Categoria)
 admin.site.register(Editora)
+admin.site.register(Livro)
--- livraria/models.py
+++ livraria/models.py
@@ -26,3 +26,21 @@ class Editora(models.Model):
 
     def __str__(self):
         return self.nome
+
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=32, null=True, blank=True)
+    quantidade = models.IntegerField(default=0, null=True, blank=True)
+    preco = models.DecimalField(
+        max_digits=7, decimal_places=2, default=0, null=True, blank=True
+    )
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="livros"
+    )
+    editora = models.ForeignKey(
+        Editora, on_delete=models.PROTECT, related_name="livros"
+    )
+
+    def __str__(self):
+        return f"{self.titulo} ({self.quantidade})"
